4b18ced32b60d789637104f6a6651118438df9ee
Devin <dolsen1@pkglobal.com>
2025-06-30 23:24:18 -0700

    Dev Release
    - Support both tap and tapAsync

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ class WebpackHooks {
 
   apply(compiler) {
     Object.keys(this.hooks).map((i) => {
-      compiler.hooks[i].tap('WebpackHooks', (param1, param2 = undefined, param3 = undefined) => {
-        this.hooks[i](param1, param2, param3);
+      if (!compiler.hooks[i]) {
+        console.error(`\x1b[31mWebpackHooks: Unknown hook \x1b[37m'${i}' \x1b[31mwas used.\nSee https://webpack.js.org/api/compiler-hooks/ for available hooks.\x1b[0m`);
+        return false;
+      }
+
+      const isAsync = this.hooks[i].constructor.name === 'AsyncFunction';
+      compiler.hooks[i][(isAsync) ? 'tapAsync' : 'tap']('WebpackHooks', (param1, ...args) => {
+        this.hooks[i](param1, ...args);
       });
     });
   }
